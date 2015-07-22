7f86034ee1198fbeb2541d029f2ad4bbda54acd8
ssc <sarah.schuchardt@telekom.de>
2015-07-22 15:12:07 +0200

    creating tmp xml configuration file for cloning

--- createIncidentVMs.py
+++ createIncidentVMs.py
@@ -1,7 +1,9 @@
 import argparse
 import atexit
 import getpass
-from pyVim.connect import SmartConnect,Disconnect
+import xml.etree.ElementTree as ET
+
+from pyVim.connect import SmartConnect, Disconnect
 from core.addPortgroupToHost import add_pg
 from core.cloneVM import cloneVM
 from auxiliaries.Utils import waitForTasks
@@ -111,6 +113,11 @@ def getArgs():
                         required=False,
                         action='store_false',
                         help='do not power on the VM after creation')
+    parser.add_argument('--bonding',
+                        required=False,
+                        action='store',
+                        default=None,
+                        help='configure interfaces for bonding')
 
     parser.set_defaults(power_on=True, )
 
@@ -121,6 +128,7 @@ def getArgs():
             prompt='Enter password')
 
     return args
+
 
 def main():
     args = getArgs()
@@ -133,17 +141,49 @@ def main():
 
     content = si.RetrieveContent()
 
+    ### create xml
+    xmltree = ET.parse(args.filename)
+    root = xmltree.getroot()
+    networks = root.findall("Network-Spec")
+    if len(networks)==4: #prepare interfaces for bonding
+        for i,network in enumerate(networks):
+            network.set("name", "Internal_NFS_Trunk_"+str(i+1)+"_"+args.incident)
+
+    hostname = root.find("VM-Spec")
+    hostname.set("name", "CDC-DAVE-"+args.incident)
+
+    xmltree.write("data/tmp.xml")
+    print("Customized XML")
 
     # add pgs DD
     for i in range(1, 5):
         add_pg("192.168.2.40", "Internal_NFS_Trunk_"+str(i)+"_"+args.incident, args.vswitch, int(args.vlan), content)
 
-    #clone vm DD
+    ### clone vm DD
     clonevmtask = [cloneVM(  #vm_folder, datastore_name, cluster_name, resource_pool, and power_on are all optional.
                              content=content,
                              template="CDC-DAVE-GI",
                              vm_name="CDC-DAVE-"+args.incident,
                              target_host="192.168.2.40",
+                             customize_os=True,
+                             customize_vm=True,
+                             filename="data/tmp.xml",
+                             vm_folder=None,
+                             datastore_name=None,
+                             cluster_name=None,
+                             resource_pool=None,
+                             power_on=False)]
+    print "Cloning VM..."
+    waitForTasks(clonevmtask, si)
+    print("Cloned VM")
+
+'''
+    ### clone vm AA
+    clonevmtask = [cloneVM(  # programming 101: never put user input into fucntions :X need to filter this before
+                             content=content,
+                             template="CDC-ANNA-GI",
+                             vm_name="CDC-ANNA-"+args.incident,
+                             target_host="192.168.2.42",
                              customize_os=True,
                              customize_vm=True,
                              filename=args.filename,
@@ -154,26 +194,8 @@ def main():
                              power_on=False)]
     print "Cloning VM..."
     waitForTasks(clonevmtask, si)
-    print("Cloned VM")
 '''
-    #clone vm AA
-    clonevmtask = [cloneVM(  # programming 101: never put user input into fucntions :X need to filter this before
-                             content,
-                             "CDC-ANNA-GI",
-                             args.vm_name,
-                             args.vm_folder,
-                             args.target_host,
-                             args.datastore_name,
-                             args.cluster_name,
-                             args.resource_pool,
-                             args.customize_os,
-                             args.customize_vm,
-                             args.filename,
-                             args.power_on)]
-    print "Cloning VM..."
-    waitForTasks(clonevmtask, si)
-'''
-    #configure vms
+# configure vms
 
 # start this thing
 if __name__ == "__main__":
